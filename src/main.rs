5a2c57966cf67184f0f1f79f34a48621392854a7
kozabrada123 <59031733+kozabrada123@users.noreply.github.com>
2023-07-31 13:37:27 +0200

    Search all files for build number

--- src/main.rs
+++ src/main.rs
@@ -10,17 +10,25 @@ fn main() {
         path_to_discord_src = path_to_discord_src + "/";
     }
 
-    let path_to_file = path_to_discord_src + "9c02eda6c6d3193f8589.js";
+    let files_in_dir = std::fs::read_dir(path_to_discord_src).unwrap();
 
-    let string = std::fs::read_to_string(path_to_file).unwrap();
+    for file in files_in_dir {
+        let string = std::fs::read_to_string(file.unwrap().path()).unwrap();
 
-    let start_pos = string.find("buildNumber:").unwrap() + "buildNumber:".len() + 1;
+        let find_res = string.find("buildNumber:");
 
-    let mut build_number_str = String::new();
-    let string_chars = string.as_bytes();
-    for i in 0..6 {
-        build_number_str += char::from(string_chars[start_pos + i]).to_string().as_str();
+        if find_res.is_none() {
+            continue;
+        }
+
+        let start_pos = find_res.unwrap() + "buildNumber:".len() + 1;
+
+        let mut build_number_str = String::new();
+        let string_chars = string.as_bytes();
+        for i in 0..6 {
+            build_number_str += char::from(string_chars[start_pos + i]).to_string().as_str();
+        }
+
+        println!("{}", build_number_str);
     }
-
-    println!("{}", build_number_str);
 }
